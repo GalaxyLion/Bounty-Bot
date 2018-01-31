21a01f61b84b9b373b44e05b60f8171bc83f4a35
GalaxyLion <galaxylion774@gmail.com>
2018-01-31 23:37:42 +0200

    Amazing commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-
 public class Main {
 
     public static void main(String[] args) {

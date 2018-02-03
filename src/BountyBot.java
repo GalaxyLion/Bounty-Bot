e5c950098035c0536f3ffb0cd24e885f80c57050
GalaxyLion <galaxylion774@gmail.com>
2018-02-03 23:09:35 +0200

    Add files via upload

--- src/BountyBot.java
+++ src/BountyBot.java
@@ -3,33 +3,38 @@ import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+
 public class BountyBot extends TelegramLongPollingBot {
 
-    @Override
+
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()){
 
             String messagText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
-            SendMessage message = new SendMessage().setChatId(chatId).setText(messagText);
-
-            try{
-                execute(message);
-            }catch (TelegramApiException e){
-                e.printStackTrace();
+            if (messagText.equals("/start")) {
+                // User send /start
+                SendMessage message = new SendMessage() // Create a message object object
+                        .setChatId(chatId)
+                        .setText("Добро пожаловать в BountyAirDrop_Bot." +
+                                "Это самый простой способ узнать информацию об актуальных Bounty компаниях, различных AirDrop, а также криптовалютных кранах");
+                try {
+                    execute(message); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
 
         }
 
     }
 
-    @Override
+
     public String getBotUsername() {
         return "Bounty Bot";
     }
 
-    @Override
     public String getBotToken() {
         return "524626702:AAHt_wLkXtHeXm3-dCguoMrSNwqhmz_Vy0M";
     }
